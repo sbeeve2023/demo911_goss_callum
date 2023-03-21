c38ff5c183339fdd5f736a2837fc7c84e7cd50af
Callum Goss <cgoss3@my.bcit.ca>
2023-03-21 08:48:25 -0700

    added everything

--- scripts/firebaseAPI_template.js
+++ scripts/firebaseAPI_template.js
@@ -2,8 +2,13 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    //keys go here 
-  
+    apiKey: "AIzaSyDU4lpAKKL9bzNm5j0i-LX_lFvFJedWZv8",
+    authDomain: "comp1800-202321.firebaseapp.com",
+    projectId: "comp1800-202321",
+    storageBucket: "comp1800-202321.appspot.com",
+    messagingSenderId: "1016892848666",
+    appId: "1:1016892848666:web:20301b0a57d05a84a22fa3"
+
 };
 
 //--------------------------------------------
